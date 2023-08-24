1d4a5bd70ac3a56f795b763309a76a093657e435
pranav.pawale <pranavpawale9146@gmail.com>
2023-08-24 12:26:33 +0530

    changes(responsive)

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -7,7 +7,10 @@ function HomePage() {
     <>
 
       <div className="main">
+ 
+
         <div className="left">
+        <div className="mstore">     <img src={store} alt="" style={{ width: "80%", height: "auto"}} /></div>
           <h1>Welcome to our wholesale Retailer distribution
             Enterprise</h1>
           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</p>
@@ -18,14 +21,11 @@ function HomePage() {
         </div>
 
 
-
-
-
-
         <div className="right">
 
-
+<div className="store">
           <img src={store} alt="" style={{ width: "80%", height: "auto"}} />
+          </div>
 
 
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,72 @@
-import React from 'react'
-import "../styles/Navbar.css"
-import logo from "../images/logo.svg"
+// import React from 'react'
+// import "../styles/Navbar.css"
+// import logo from "../images/logo.svg"
 
 
-function Navbar() {
-  return (
-    <>
-   <div className="navbar">
-    <img src={logo} alt="" />
-  <a className="nav-link nav-link-ltr active" href="#">Home</a>
-  <a className="nav-link nav-link-ltr" href="#">History</a>
-  <a className="nav-link nav-link-ltr" href="#">Products</a>
-  <a className="nav-link nav-link-ltr" href="#">Profile</a>
-  <a className="nav-link nav-link-ltr" href="#">Contact us</a>
+// function Navbar() {
+//   return (
+//     <>
+//    <div className="navbar">
+//     <img src={logo} alt="" />
+//   <a className="nav-link nav-link-ltr active" href="#">Home</a>
+//   <a className="nav-link nav-link-ltr" href="#">History</a>
+//   <a className="nav-link nav-link-ltr" href="#">Products</a>
+//   <a className="nav-link nav-link-ltr" href="#">Profile</a>
+//   <a className="nav-link nav-link-ltr" href="#">Contact us</a>
 
-</div>
+// </div>
 
 
     
-    </>
-  )
+//     </>
+//   )
+// }
+
+// export default Navbar
+import React, { useState } from 'react';
+import "../styles/Navbar.css";
+import logo from "../images/logo.svg";
+import MenuIcon from '@mui/icons-material/Menu'
+import { Typography } from '@mui/material';
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import { Twitter } from '@mui/icons-material';
+import { LinkedIn } from '@mui/icons-material';
+import { Instagram } from '@mui/icons-material';
+
+
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+      <img src={logo} alt="" />
+      <Typography className="swaraj" style={{fontWeight:"550",font:"lato",fontSize:"17px"}}>Swaraj enterprise</Typography>
+      <button className="menu-button" onClick={toggleMenu}>
+        <MenuIcon/>
+      </button>
+      <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+        <a className="nav-link nav-link-ltr active" href="#">Home</a>
+        <a className="nav-link nav-link-ltr" >History</a>
+        <a className="nav-link nav-link-ltr" >Products</a>
+        <a className="nav-link nav-link-ltr" >Profile</a>
+        <a className="nav-link nav-link-ltr" >Contact us</a>
+        <a className="nav" >Social Media</a>
+        <div style={{display:"flex",justifyContent:"row"}}>     
+           <div className="facebook" style={{marginLeft:"2%",marginTop:"1%"}}><FacebookOutlinedIcon ></FacebookOutlinedIcon></div>
+        <div className="twitter" style={{marginLeft:"2%",marginTop:"1%"}}><Twitter/></div>
+        <div className="LinkedIn" style={{marginLeft:"2%",marginTop:"1%"}}><LinkedIn/></div>
+        <div className="Instagram" style={{marginLeft:"2%",marginTop:"1%"}}><Instagram/></div>
+        </div>
+
+
+    </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
